4a07254e68336e1b0bdc81adcde194ab2178ebcd
Suvojit2020 <53915542+Suvojit2020@users.noreply.github.com>
2022-11-10 17:35:55 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,23 @@
+class world
+{
+world()
+{
+	System.out.println("Hellow");
+}
+
+void show()
+{
+	System.out.println("Hi");
+}
+}
+
+class main
+{
+	public static void main(String args[])
+	{
+		world obj=new world();
+		obj.show();
+	}
+
+
+}
